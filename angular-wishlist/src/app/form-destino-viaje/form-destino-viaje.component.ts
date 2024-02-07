19858853aaafc593a483000097b0d5613a9604e1
tatoeberlein <tomitatoeber@gmail.com>
2024-02-07 15:56:31 -0300

    validator personalizado y parametrizable

--- angular-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, Form, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { DestinoViaje } from '../models/destino-viaje.model';
 import { NgIf } from '@angular/common';
 
@@ -12,14 +12,18 @@ import { NgIf } from '@angular/common';
 })
 export class FormDestinoViajeComponent implements OnInit{
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
-
   public fg! : FormGroup
+  minLongitud = 5;
 
   constructor(fb: FormBuilder) {  
     this.onItemAdded = new EventEmitter();
 
     this.fg = new FormGroup({
-      nombre: new FormControl('', Validators.required),
+      nombre: new FormControl('', Validators.compose([
+        Validators.required,
+/*         this.nombreValidator,*/
+         this.nombreValidatorParametrizable(this.minLongitud)
+      ])),
       url: new FormControl('')
     });
 
@@ -36,6 +40,25 @@ export class FormDestinoViajeComponent implements OnInit{
     this.onItemAdded.emit(d);
     console.log(this.fg.value);
     return false;
-  }
+  }  
 
+/*   nombreValidator(control: FormControl): { [s:string]: boolean } {
+    const l = control.value.toString().trim().length;
+    if (l > 0 && l < 5) {
+      return { invalidNombre: true };
+    }
+     else {
+      return {invalidNombre: false};
+     }
+  } */
+
+  nombreValidatorParametrizable(minLong: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const l = control.value.toString().trim().length;
+      if (l > 0 && l < minLong) {
+        return { minLongNombre: true };
+      }
+      return null;
+    };
+  }
 }
