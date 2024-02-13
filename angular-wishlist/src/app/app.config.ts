25d233d916d2f1dc96ba6e66320f5f789aaa68a7
tatoeberlein <tomitatoeber@gmail.com>
2024-02-12 22:12:51 -0300

    redux devtools install

--- angular-wishlist/src/app/app.config.ts
+++ angular-wishlist/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { DestinosApiClient } from './models/destinos-api-client.model'
 
@@ -7,13 +7,15 @@ import { provideState, provideStore } from '@ngrx/store';
 import { reducerDestinosViajes } from './states/destinos-viajes/destinos-viajes.reducer';
 import { provideEffects } from '@ngrx/effects';
 import { DestinosViajesEffects } from './states/destinos-viajes/destinos-viajes.effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     DestinosApiClient,
     provideStore(),
-    provideState({name: 'destinos', reducer: reducerDestinosViajes}),
-    provideEffects(DestinosViajesEffects)
-  ]
+    provideState({ name: 'destinos', reducer: reducerDestinosViajes }),
+    provideEffects(DestinosViajesEffects),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
